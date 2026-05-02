bc0f16308525d89c1ed07fda966e0c27f2a8510a
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-02 12:41:23 -0300

    initial commit

---
+++ src/main/java/com/om1cael/starpassapi/StarPassApiApplication.java
@@ -0,0 +1,13 @@
+package com.om1cael.starpassapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarPassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarPassApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/om1cael/starpassapi/StarPassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om1cael.starpassapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarPassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
