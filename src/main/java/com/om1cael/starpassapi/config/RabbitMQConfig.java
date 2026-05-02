92ae1bea620340a13a013282259bcff62d8b0661
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-02 13:43:44 -0300

    chore: setup rabbitmq config

---
+++ src/main/java/com/om1cael/starpassapi/config/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package com.om1cael.starpassapi.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange("orders.topic");
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue("payment.request");
+    }
+
+    @Bean
+    public Binding binding(Queue orderQueue, TopicExchange orderExchange) {
+        return BindingBuilder
+                .bind(orderQueue)
+                .to(orderExchange)
+                .with("order.created");
+
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
